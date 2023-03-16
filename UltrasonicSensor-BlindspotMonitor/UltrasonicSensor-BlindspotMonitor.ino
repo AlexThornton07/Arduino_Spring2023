f7cb3aa3bafd43e0c3f4e1a73550bf83b514840c
Alex Thornton <alexander.thornton07@gmail.com>
2023-03-16 14:07:49 -0400

    Initial Blind Spot Monitor Commit

---
+++ UltrasonicSensor-BlindspotMonitor/UltrasonicSensor-BlindspotMonitor.ino
@@ -0,0 +1,19 @@
+//Programmer:Alex Thornton
+//Date: 2.16.23
+//Program: Blind Spot Monitor
+
+/*
+ * This program will determine the distance of approaching 
+ * vehicles that are in your blind spot and turn on a warning light
+ */
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
