a81f0c8076009018d72b8778d991f0b651ec8d2b
Alex Thornton <alexander.thornton07@gmail.com>
2023-03-22 13:44:45 -0400

    Copy and Paste Code from Resource

--- UltrasonicSensor-BlindspotMonitor/UltrasonicSensor-BlindspotMonitor.ino
+++ UltrasonicSensor-BlindspotMonitor/UltrasonicSensor-BlindspotMonitor.ino
@@ -1,6 +1,7 @@
 //Programmer:Alex Thornton
-//Date: 2.16.23
-//Program: Blind Spot Monitor
+//Date:2.16.23
+//Program:Blind Spot Monitor
+//Resource:https://projecthub.arduino.cc/Krepak/a6ea3ad5-fd23-49c9-a96c-5e6fcf97f2cc?ref=search&ref_id=red%20green%20yellow%20light%20echo%20sensor&offset=1
 
 /*
  * This program will determine the distance of approaching 
@@ -8,12 +9,64 @@
  */
 
 
-void setup() {
-  // put your setup code here, to run once:
-
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-
-}
+#define trigPin 2
+2#define echoPin 3
+3#define LEDlampRed 4
+4#define  LEDlampYellow 5
+5#define LEDlampGreen 6 
+6#define soundbuzzer 7
+7int sound  = 500;
+8
+9
+10void setup() {
+11  Serial.begin (9600);
+12  pinMode(trigPin,  OUTPUT);
+13  pinMode(echoPin, INPUT);
+14  pinMode(LEDlampRed, OUTPUT);
+15  pinMode(LEDlampYellow,  OUTPUT);
+16  pinMode(LEDlampGreen, OUTPUT);
+17  pinMode(soundbuzzer, OUTPUT);
+18}
+19void  loop() {
+20  long durationindigit, distanceincm;
+21  digitalWrite(trigPin, LOW);  
+22  delayMicroseconds(2);
+23  digitalWrite(trigPin, HIGH);
+24  delayMicroseconds(10);
+25  digitalWrite(trigPin, LOW);
+26  durationindigit = pulseIn(echoPin, HIGH);
+27  distanceincm = (durationindigit/5) / 29.1;
+28 
+29  if (distanceincm < 50) {
+30      digitalWrite(LEDlampGreen, HIGH);
+31}
+32  else {
+33      digitalWrite(LEDlampGreen,  LOW);
+34  }
+35  
+36  if (distance < 20) {
+37    digitalWrite(LEDlampYellow,  HIGH);
+38}
+39  else {
+40    digitalWrite(LEDlampYellow,LOW);
+41  }
+42  if (distance  < 5) {
+43    digitalWrite(LEDlampRed, HIGH);
+44    sound = 1000;
+45}
+46  else  {
+47    digitalWrite(LEDlampRed,LOW);
+48  }
+49 
+50  if (distanceincm > 5 ||  distanceinsm <= 0){
+51    Serial.println("Outside the permissible range of distances");
+52    noTone(soundbuzzer);
+53  }
+54  else {
+55    Serial.print(distance);
+56    Serial.println("  cm");
+57    tone(buzzer, sound);
+58  }
+59  
+60  delay(300);
+61}
